0b2feef99f0c963dc70e0327f00d7a0dca038872
sodantoine <50483395+sodantoine@users.noreply.github.com>
2021-08-22 20:33:33 +0000

    antoine1

---
+++ database/factories/RoleUserFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\role_user;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RoleUserFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = role_user::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
